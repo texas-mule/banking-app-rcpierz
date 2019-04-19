508bf2c5d626b8c371df839156a6ddc6939849d7
RCP <RCP@10.182.5.14>
2019-04-19 12:16:13 -0500

    blah

--- src/main/java/com/revature/Main.java
+++ src/main/java/com/revature/Main.java
@@ -17,35 +17,40 @@ public class Main {
 		System.out.println("Welcome to Texas Mule Banking Systems.+\n"
 				+ "Please enter your username (Case Sensitive). If you do not have an account, enter \"guest\". Enter exit to close the application");
 
-		//Continue processing input until user enters 'exit'
+		// Continue processing input until user enters 'exit'
 		while (!(loginUsername = scanIn.nextLine()).equals("exit\n") || accessLevel == 0) {
-			
-			//Account Creation Option
+
+			// Account Creation Option
 			if (loginUsername.equalsIgnoreCase("GUEST")) {
-				currentUser = Person.createNew();	//Call Person creation method.
-				personnel.add(currentUser);			//Add new user to personnel directory
-				System.out.print("Thank you for creating a new account. You may now log in.\nPlease enter your username: ");
+				while (!(Person.findUserInDatabase(loginUsername, personnel).id == 0)) {
+					System.out.println("Username already exists. Please enter a new username.");
+					loginUsername = scanIn.nextLine();
+				}
+				currentUser = Person.createNew(); // Call Person creation method.
+				personnel.add(currentUser); // Add new user to personnel directory
+				System.out.print(
+						"Thank you for creating a new account. You may now log in.\nPlease enter your username: ");
 				loginUsername = scanIn.nextLine();
 			}
-			
-			//Search for user
+
+			// Search for user
 			while (Person.findUserInDatabase(loginUsername, personnel).getUsername().equals("")) {
 				System.out.print("Username not found. Please re-enter your username: ");
 				loginUsername = scanIn.nextLine();
 			}
-			
-			//Prompt Password
+
+			// Prompt Password
 			System.out.print("Please enter your password: ");
 			loginPassword = scanIn.nextLine();
-			
-			//Check password
+
+			// Check password
 			while (!currentUser.checkPassword(loginPassword)) {
 				System.out.print("Incorrect password. Please re-enter your password: ");
 				loginPassword = scanIn.nextLine();
 			}
-			
-			//User is logged in
-			System.out.println("Thank you for logging in, Mr/Mrs. "+currentUser.getlName());
+
+			// User is logged in
+			System.out.println("Thank you for logging in, Mr/Mrs. " + currentUser.getlName());
 
 		} // end While loop
 		System.out.println("Access has been granted, or you are exiting. Successfully exited While loop");
